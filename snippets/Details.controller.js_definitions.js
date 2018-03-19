118278039e876934fcaa5659b43c550c5b96716a
I029162 <maria.trinidad.martinez.gea@sap.com>
2018-03-19 02:32:44 +0100

    SAPUI5 application sources and also snippets

---
+++ snippets/Details.controller.js_definitions.js
@@ -0,0 +1,9 @@
+sap.ui.define(["sap/ui/core/mvc/Controller",
+		"sap/m/MessageToast",
+		"sap/ui/core/Fragment",
+		'sap/ui/model/Filter',
+		'sap/ui/model/json/JSONModel',
+		'jquery.sap.global'
+	],
+	function(Controller, MessageToast, Fragment, Filter, JSONModel, jQuery) {
+		"use strict";
